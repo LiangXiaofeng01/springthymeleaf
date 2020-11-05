2a0554e14490d2cbc02fbdb9e8c387754c900adb
lyj <123@qq.com>
2020-11-05 23:05:38 +0800

    项目前端模板

---
+++ src/main/java/com/SpringBootStarttest.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarttest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarttest.class,args);
+    }
+
+
+}
